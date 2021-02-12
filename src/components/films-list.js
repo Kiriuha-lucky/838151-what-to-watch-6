41b0ebf92faac0b39a5ea89fa654e25765db46b4
Kiriuha-lucky <Kiriuha777@gmail.com>
2021-02-12 15:21:12 +0300

    activeFilm in props

--- src/components/films-list.js
+++ src/components/films-list.js
@@ -4,8 +4,11 @@ import FilmCard from './film-card';
 const FilmsList = (props) => {
   let [activeFilm, setActiveFilm] = React.useState(0);
   const {films} = props;
+  const onMouseEnter = (id) => {
+    return setActiveFilm(activeFilm = id);
+  };
   return films.map(({id, name, posterImage}) =>
-    <FilmCard key={id} id={id} name={name} posterImage={posterImage} onMouseEnter={() => setActiveFilm(activeFilm)} />);
+    <FilmCard key={id} id={id} name={name} posterImage={posterImage} activeFilm={activeFilm} onMouseEnter={()=>onMouseEnter(id)} />);
 };
 
 export default FilmsList;
