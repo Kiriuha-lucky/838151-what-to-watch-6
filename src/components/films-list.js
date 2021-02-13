48f54f15f09989b76d50d1d557f81489043c2e76
Kiriuha-lucky <Kiriuha777@gmail.com>
2021-02-13 23:09:50 +0300

    change let to const

--- src/components/films-list.js
+++ src/components/films-list.js
@@ -2,10 +2,10 @@ import React from 'react';
 import FilmCard from './film-card';
 
 const FilmsList = (props) => {
-  let [activeFilm, setActiveFilm] = React.useState(0);
+  const [activeFilm, setActiveFilm] = React.useState(0);
   const {films} = props;
   const onMouseEnter = (id) => {
-    return setActiveFilm(activeFilm = id);
+    return setActiveFilm(id);
   };
   return films.map(({id, name, posterImage}) =>
     <FilmCard key={id} id={id} name={name} posterImage={posterImage} activeFilm={activeFilm} onMouseEnter={()=>onMouseEnter(id)} />);
